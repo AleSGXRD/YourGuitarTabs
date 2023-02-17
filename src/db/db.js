5a09ffcb470b10ef6b619cc7dc4b40ea095f1379
AlejandroXRD <106454456+AleSGXRD@users.noreply.github.com>
2023-02-17 17:09:49 -0500

    conect

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,7 @@
 import mysql from "mysql";
 import { DB_HOST,DB_NAME,DB_PASSWORD,DB_USER,DB_PORT } from "../config.js";
 
-const db = mysql.createConnection({
+const db = await mysql.createConnection({
     user:DB_USER,
     password:DB_PASSWORD,
     host:DB_HOST,
