5ace497707411ef2e3c2003845a95d9c110ab6ba
AlejandroXRD <106454456+AleSGXRD@users.noreply.github.com>
2023-02-17 17:29:50 -0500

    again

--- src/db/db.js
+++ src/db/db.js
@@ -1,20 +1,12 @@
-import mysql from "mysql";
+import {createPool} from "mysql";
 import { DB_HOST,DB_NAME,DB_PASSWORD,DB_USER,DB_PORT } from "../config.js";
 
-const db = await mysql.createConnection({
+const db = createPool({
     user:DB_USER,
     password:DB_PASSWORD,
     host:DB_HOST,
     port:   DB_PORT,
     database:DB_NAME
-})
-
-db.connect((error)=>{
-    if(error){
-        console.log(error);
-    }
-    
-    console.log("connect");
 })
 
 export default db;
